a7d5caf02a18721896dae8a32544fdc1285988a8
Trenton Taylor <trenton.taylor.email@gmail.com>
2025-12-01 17:57:29 -0600

    1.1.4

---
+++ PlugCoreExamplePlugin/src/main/java/io/plugcore/plugCoreExamplePlugin/PlugCoreExamplePlugin.java
@@ -0,0 +1,26 @@
+package io.plugcore.plugCoreExamplePlugin;
+
+import io.plugcore.plugCore.PlugCore;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class PlugCoreExamplePlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        try {
+            PlugCore.requireAuthorization(this);
+            getLogger().info("PlugCoreExamplePlugin enabled - authorization check scheduled.");
+        } catch (NoClassDefFoundError e) {
+            getLogger().severe("PlugCore not found! Download from https://plugcore.io");
+            getServer().getPluginManager().disablePlugin(this);
+        } catch (Exception e) {
+            getLogger().severe("Authorization error: " + e.getMessage());
+            getServer().getPluginManager().disablePlugin(this);
+        }
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("PlugCoreExamplePlugin disabled.");
+    }
+}
--- src/main/java/io/plugcore/plugCore/PlugCore.java
+++ src/main/java/io/plugcore/plugCore/PlugCore.java
@@ -17,6 +17,86 @@ public final class PlugCore extends JavaPlugin {
     private ValidationService validationService;
     private PluginDependencyService dependencyService;
 
+    public static boolean isServerLinked() {
+        if (instance == null) {
+            return false;
+        }
+        if (instance.getValidationService() == null) {
+            return false;
+        }
+        ServerLinkData linkData = instance.getValidationService().getCurrentLinkData();
+        if (linkData == null) {
+            return false;
+        }
+        return linkData.isLinked();
+    }
+
+    public static CompletableFuture<Boolean> isPluginAuthorizedByHash(String jarHash) {
+        if (instance == null) {
+            return CompletableFuture.completedFuture(false);
+        }
+        return instance.getValidationService().isPluginAuthorized(jarHash);
+    }
+
+    public static CompletableFuture<Boolean> validateServer() {
+        if (instance == null) {
+            return CompletableFuture.completedFuture(false);
+        }
+        return instance.getValidationService().validateServerLink();
+    }
+
+    public static boolean requireAuthorization(org.bukkit.plugin.Plugin plugin) {
+        org.bukkit.Bukkit.getScheduler().runTaskLater(plugin, () -> {
+            boolean serverLinked = isServerLinked();
+            if (!serverLinked) {
+                plugin.getLogger().severe("Server not linked to PlugCore!");
+                plugin.getLogger().severe("This plugin cannot run on unlinked servers.");
+                plugin.getLogger().severe("Link your server: /plugcore link <token>");
+                plugin.getLogger().severe("Get your token from your account page!");
+                org.bukkit.Bukkit.getScheduler().runTask(plugin, () -> {
+                    org.bukkit.Bukkit.getPluginManager().disablePlugin(plugin);
+                });
+                return;
+            }
+
+            String jarHash = instance.getDependencyService().calculatePluginJarHash(plugin);
+            if (jarHash == null) {
+                plugin.getLogger().severe("Failed to calculate plugin hash!");
+                org.bukkit.Bukkit.getScheduler().runTask(plugin, () -> {
+                    org.bukkit.Bukkit.getPluginManager().disablePlugin(plugin);
+                });
+                return;
+            }
+
+            instance.getValidationService().isPluginAuthorized(jarHash).thenAccept(authorized -> {
+                if (!authorized) {
+                    plugin.getLogger().severe("This plugin is NOT authorized!");
+                    plugin.getLogger().severe("You have not purchased this plugin.");
+                    plugin.getLogger().severe("Purchase at plugcore.io");
+                    org.bukkit.Bukkit.getScheduler().runTask(plugin, () -> {
+                        org.bukkit.Bukkit.getPluginManager().disablePlugin(plugin);
+                    });
+                } else {
+                    plugin.getLogger().info("Plugin authorized!");
+                }
+            }).exceptionally(throwable -> {
+                plugin.getLogger().severe("Failed to validate authorization: " + throwable.getMessage());
+                org.bukkit.Bukkit.getScheduler().runTask(plugin, () -> {
+                    org.bukkit.Bukkit.getPluginManager().disablePlugin(plugin);
+                });
+                return null;
+            });
+        }, 200L);
+        return true;
+    }
+
+    public static PlugCore getInstance() {
+        if (instance == null) {
+            throw new IllegalStateException("PlugCore is not available. Make sure PlugCore is loaded and enabled before using the API.");
+        }
+        return instance;
+    }
+
     @Override
     public void onLoad() {
         instance = this;
@@ -38,7 +118,6 @@ public final class PlugCore extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        onLoad();
         PlugCoreCommand command = new PlugCoreCommand(validationService, dependencyService);
         if (getCommand("plugcore") != null) {
             getCommand("plugcore").setExecutor(command);
@@ -73,99 +152,5 @@ public final class PlugCore extends JavaPlugin {
 
     public PluginDependencyService getDependencyService() {
         return dependencyService;
-    }
-
-    public static boolean isServerLinked() {
-        System.out.println("[PlugCore] isServerLinked() called");
-        System.out.println("[PlugCore] instance is null: " + (instance == null));
-        if (instance == null) {
-            System.out.println("[PlugCore] Returning false - instance is null");
-            return false;
-        }
-        System.out.println("[PlugCore] instance exists: " + instance);
-        System.out.println("[PlugCore] ValidationService is null: " + (instance.getValidationService() == null));
-        if (instance.getValidationService() == null) {
-            System.out.println("[PlugCore] Returning false - ValidationService is null");
-            return false;
-        }
-        ServerLinkData linkData = instance.getValidationService().getCurrentLinkData();
-        System.out.println("[PlugCore] LinkData is null: " + (linkData == null));
-        if (linkData == null) {
-            System.out.println("[PlugCore] Returning false - LinkData is null");
-            return false;
-        }
-        boolean linked = linkData.isLinked();
-        System.out.println("[PlugCore] linkData.isLinked(): " + linked);
-        return linked;
-    }
-
-    public static CompletableFuture<Boolean> isPluginAuthorizedByHash(String jarHash) {
-        if (instance == null) {
-            return CompletableFuture.completedFuture(false);
-        }
-        return instance.getValidationService().isPluginAuthorized(jarHash);
-    }
-
-    public static CompletableFuture<Boolean> validateServer() {
-        if (instance == null) {
-            return CompletableFuture.completedFuture(false);
-        }
-        return instance.getValidationService().validateServerLink();
-    }
-
-    public static boolean requireAuthorization(org.bukkit.plugin.Plugin plugin) {
-        org.bukkit.Bukkit.getScheduler().runTaskLater(plugin, () -> {
-            plugin.getLogger().info("Starting authorization check for plugin: " + plugin.getName());
-            boolean serverLinked = isServerLinked();
-            plugin.getLogger().info("isServerLinked() result: " + serverLinked);
-            if (!serverLinked) {
-                plugin.getLogger().severe("Server not linked to PlugCore!");
-                plugin.getLogger().severe("This plugin cannot run on unlinked servers.");
-                plugin.getLogger().severe("Link your server: /plugcore link <token>");
-                plugin.getLogger().severe("Get your token from your account page!");
-                org.bukkit.Bukkit.getScheduler().runTask(plugin, () -> {
-                    org.bukkit.Bukkit.getPluginManager().disablePlugin(plugin);
-                });
-                return;
-            }
-
-            String jarHash = instance.getDependencyService().calculatePluginJarHash(plugin);
-            plugin.getLogger().info("Calculated jarHash: " + jarHash);
-            if (jarHash == null) {
-                plugin.getLogger().severe("Failed to calculate plugin hash!");
-                org.bukkit.Bukkit.getScheduler().runTask(plugin, () -> {
-                    org.bukkit.Bukkit.getPluginManager().disablePlugin(plugin);
-                });
-                return;
-            }
-
-            instance.getValidationService().isPluginAuthorized(jarHash).thenAccept(authorized -> {
-                plugin.getLogger().info("Authorization result for hash " + jarHash + ": " + authorized);
-                if (!authorized) {
-                    plugin.getLogger().severe("This plugin is NOT authorized!");
-                    plugin.getLogger().severe("You have not purchased this plugin.");
-                    plugin.getLogger().severe("Purchase at plugcore.io");
-                    org.bukkit.Bukkit.getScheduler().runTask(plugin, () -> {
-                        org.bukkit.Bukkit.getPluginManager().disablePlugin(plugin);
-                    });
-                } else {
-                    plugin.getLogger().info("Plugin authorized! ✓");
-                }
-            }).exceptionally(throwable -> {
-                plugin.getLogger().severe("Failed to validate authorization: " + throwable.getMessage());
-                org.bukkit.Bukkit.getScheduler().runTask(plugin, () -> {
-                    org.bukkit.Bukkit.getPluginManager().disablePlugin(plugin);
-                });
-                return null;
-            });
-        }, 200L);
-        return true;
-    }
-
-    public static PlugCore getInstance() {
-        if (instance == null) {
-            throw new IllegalStateException("PlugCore is not available. Make sure PlugCore is loaded and enabled before using the API.");
-        }
-        return instance;
     }
 }
